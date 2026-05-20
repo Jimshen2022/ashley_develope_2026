6fe9623012cac3b3fb44363c69659f334f4501a9
F22SHEN <jimshen2008@163.com>
2026-05-20 11:14:32 +0700

    Auto-sync: 2026-05-20 11:14:32

--- 000_hj_sql_schema/AFI Issue Check/replenishment_issue_check.sql
+++ 000_hj_sql_schema/AFI Issue Check/replenishment_issue_check.sql
@@ -7,3 +7,7 @@ select top 10 * from t_item_uom where item_number = 'A2000665'
 select top 10 * from t_fwd_pick where item_number = 'A2000665'
 select top 10 * from t_loc_pallet_capacity where location_id = 'A3012EA1'
 select top 1000 * from t_work_q where item_number = 'A2000665'
+
+
+select top 10 * from t_loc_pallet_capacity where location_id = 'A3012EA1'
+select * from t_loc_pallet_capacity where capacity >50 and location_id like 'A3%'
